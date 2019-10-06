14c67abe6897fb63b81417849a12379e101ce747
Frank S. Bara <fbara1@yahoo.com>
2019-10-05 23:18:41 -0500

    Added OrderView to begin building the order.

---
+++ iDine/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Frank Bara on 10/5/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
