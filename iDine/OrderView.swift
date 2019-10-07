74b13ff9852c73545deda9173b57dabbe8587281
Frank S. Bara <fbara1@yahoo.com>
2019-10-06 21:55:21 -0500

    Final commit.

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -21,19 +21,24 @@ struct OrderView: View {
                             Spacer()
                             Text("$\(item.price)")
                         }
-                    }
+                    }.onDelete(perform: deleteItems)
                 }
                 
                 Section {
                     NavigationLink(destination:
-                    Text("Check Out")) {
+                    CheckoutView()) {
                         Text("Place Order")
                     }
-                }
+                }.disabled(order.items.isEmpty)
             }
         .navigationBarTitle("Order")
         .listStyle(GroupedListStyle())
+        .navigationBarItems(trailing: EditButton())
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
